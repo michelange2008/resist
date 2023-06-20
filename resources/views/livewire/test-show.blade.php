89eeb413ea1a53e6786703b0fe4f88de6abe7fda
michelange2008 <michelange@wanadoo.fr>
2023-06-20 06:28:43 +0200

    idem

--- resources/views/livewire/test-show.blade.php
+++ resources/views/livewire/test-show.blade.php
@@ -7,6 +7,7 @@
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            
             <table class="min-w-full text-left text-sm font-light">
                 <thead class="border-b font-medium">
                     <th scope="col" class="px-6 py-4">Date</th>
@@ -15,12 +16,14 @@
                     <th scope="col" class="px-6 py-4">Espece</th>
                     <th scope="col" class="px-6 py-4">Efficacité</th>
                     <th scope="col" class="px-6 py-4">Nombre d'animaux</th>
+                    <th scope="col" class="px-6 py-4">Voir</th>
+                    <th scope="col" class="px-6 py-4">Suppr</th>
                 </thead>
                 <tbody>
                     @foreach ($tests as $test)
                         <tr
-                            @if ($test->efficacite < 95) class="border-b bg-red-400"
-                            @else class="border-b bg-green-300" @endif>
+                            @if ($test->efficacite < 95) class="border-b bg-red-200"
+                            @else class="border-b bg-green-200" @endif>
 
                             <td class="whitespace-nowrap px-6 py-4">{{ $test->T0 }}</td>
                             <td class="whitespace-nowrap px-6 py-4">{{ $test->anthelm->nom }}</td>
@@ -38,6 +41,10 @@
 
                             </td>
                             <td class="whitespace-nowrap px-6 py-4 font-bold text-base">{{ $test->efficacite }} %</td>
+                            <td class="whitespace-nowrap px-6 py-4 font-bold text-base"></td>
+                            <td class="whitespace-nowrap px-6 py-4 font-bold text-base text-green-900 cursor-pointer"><x-icones.link/></td>
+
+                            <td class="whitespace-nowrap px-6 py-4 font-bold text-base text-red-900 cursor-pointer"><x-icones.del/></td>
 
                         </tr>
                     @endforeach
